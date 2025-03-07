aad336d87981fd8290f8f1d0f7094e9b2d14cb51
ghostNoté <selcukbilgen@hotmail.com>
2025-03-07 17:04:22 +0300

    CreateProductRequestValidator.cs created.

---
+++ App.Services/Products/CreateProductRequestValidator.cs
@@ -0,0 +1,15 @@
+using FluentValidation;
+
+namespace Services.Products;
+
+public class CreateProductRequestValidator : AbstractValidator<CreateProductRequest>
+{
+    public CreateProductRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Ürün ismi gereklidir.")
+            .Length(3, 10).WithMessage("Ürün ismi 3-10 karakter arasında olmalıdır.");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Fiyat 0'dan büyük olmalıdır.");
+        RuleFor(x => x.Stock).InclusiveBetween(1, 100).WithMessage("Stok 1-100 arasında olmalıdır.");
+    }
+}
